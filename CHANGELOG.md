d6ae9967186acbfb549ce35ea2fa56e52ebe6963
Michael Leigeber <michael@caddis.co>
2014-04-10 15:57:25 -0500

    See changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.0.1
+
+2014-04-10
+
+- Added slight delay to initialization to ensure relationship field has loaded
+
 # 1.0.0
 
 2014-02-27
--- system/expressionengine/third_party/related_open/javascript/base.js
+++ system/expressionengine/third_party/related_open/javascript/base.js
@@ -1,27 +1,29 @@
 _.defer(function() {
-	var $relationshipOptions = $('.multiselect li');
+	_.delay(function() {
+		var $relationshipOptions = $('.multiselect li');
 
-	$relationshipOptions.click(function() {
-		_.defer(function() { 
-			bindOpen();
-		});
-	});
-
-	function bindOpen() {
-		$('.multiselect-active li').add($relationshipOptions).each(function() {
-			var $this = $(this),
-				index = $this.data('list-index'),
-				$elem = (index === undefined) ? $this : $relationshipOptions.eq(index),
-				entryId = $elem.find('input:checkbox').val(),
-				$openLink = $('<a href="' + EE.BASE + '&C=content_publish&M=entry_form&entry_id=' + entryId + '" target="_blank" class="related-open">open</a>');
-
-			$openLink.click(function(e) {
-				e.stopPropagation();
+		$relationshipOptions.click(function() {
+			_.defer(function() { 
+				bindOpen();
 			});
-
-			$this.append($openLink);
 		});
-	}
 
-	bindOpen();
+		function bindOpen() {
+			$('.multiselect-active li').add($relationshipOptions).each(function() {
+				var $this = $(this),
+					index = $this.data('list-index'),
+					$elem = (index === undefined) ? $this : $relationshipOptions.eq(index),
+					entryId = $elem.find('input:checkbox').val(),
+					$openLink = $('<a href="' + EE.BASE + '&C=content_publish&M=entry_form&entry_id=' + entryId + '" target="_blank" class="related-open">open</a>');
+
+				$openLink.click(function(e) {
+					e.stopPropagation();
+				});
+
+				$this.append($openLink);
+			});
+		}
+
+		bindOpen();
+	}, 500);
 });
